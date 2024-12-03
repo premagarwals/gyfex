da513c3949eb047b98267bbf05fd7d8dcbc0b1be
prem12937@gmail.com <prem12937@gmail.com>
2024-12-03 15:25:04 +0530

    style: add a nice header

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="bg-green-100 h-full w-screen relative overflow-x-hidden">
+      <div className="bg-slate-600 fixed z-20 h-16 w-full m-0 p-4 flex flex-col items-center justify-center text-green-200">
+        <p className='text-lg font-bold '>GYFEx</p>
+        <p className='text-xs tracing-wide'>Inspired by <a className='font-semibold' href="https://github.com/metakgp/gyfe" target='_blanck'> GYFE </a> • Made with ❤️</p>
+
+      </div>
+    </div>
   )
 }
 
